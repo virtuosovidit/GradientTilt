e13c76e4d07046608fa9fc4fa2a3310d782ba6b3
virtuosovidit <viditb@me.com>
2019-04-08 12:57:16 +0530

    v.0.02 - Gradient Color Options Added

--- GradientTilt/ViewController.swift
+++ GradientTilt/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class ViewController: UIViewController {
 
@@ -18,23 +19,49 @@ class ViewController: UIViewController {
         gradientView.mask = label
     }
     
-    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var gradientView: GradientView!
     @IBOutlet weak var label: UILabel!
     
     func addGradient() {
         let gradient = CAGradientLayer()
         gradient.frame = gradientView.bounds
+        
+        /* Option 1:
         gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
         gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
         gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
         gradientView.layer.addSublayer(gradient)
+         */
+        
+        /* Option 2:
+        gradient.colors = [UIColor(white: 0.7, alpha: 1).cgColor, UIColor(white: 0.88, alpha: 1).cgColor, UIColor(white: 0.7, alpha: 1).cgColor]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0)
+        gradient.startPoint = CGPoint(x: 0.5, y: 1)
+        gradientView.layer.addSublayer(gradient)
+        */
+        
+        gradient.colors = [UIColor(displayP3Red: 194/255, green: 141/255, blue: 34/255, alpha: 1).cgColor,
+                           UIColor(displayP3Red: 240/255, green: 194/255, blue: 42/255, alpha: 1).cgColor,
+                           UIColor(displayP3Red: 194/255, green: 141/255, blue: 34/255, alpha: 1).cgColor]
+        
+        gradient.startPoint = CGPoint(x: 1.0, y: 0.5)
+        gradient.startPoint = CGPoint(x: 0.5, y: 1)
+        
+        gradientView.layer.addSublayer(gradient)
+        
+        gradientView.gradient = gradient
     }
     
     func update() {
+        
         let motionEffectGroup = GradientMotionEffect()
         motionEffectGroup.gradient = gradientView.layer.sublayers?.last as? CAGradientLayer
+        
         gradientView.addMotionEffect(motionEffectGroup)
     }
+    
+    
+    
     
 }
 
@@ -42,8 +69,17 @@ class GradientMotionEffect: UIMotionEffect {
     var gradient: CAGradientLayer?
     
     override func keyPathsAndRelativeValues(forViewerOffset viewerOffset: UIOffset) -> [String : Any]? {
-        gradient?.startPoint.x = viewerOffset.horizontal
+        gradient?.startPoint.x = 1 + viewerOffset.horizontal
+        gradient?.startPoint.y = 0.5 + viewerOffset.vertical
+        
+        gradient?.endPoint.x = 0 + viewerOffset.horizontal
+        gradient?.endPoint.y = 1 + viewerOffset.vertical
+        
         return nil
     }
     
+}
+
+class GradientView: UIView {
+    var gradient: CAGradientLayer?
 }
