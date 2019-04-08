c9bc63e505ee441045b1f69255c465806be446e0
virtuosovidit <viditb@me.com>
2019-04-08 12:14:16 +0530

    Gradient Tilt

--- GradientTilt/ViewController.swift
+++ GradientTilt/ViewController.swift
@@ -13,8 +13,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        addGradient()
+        update()
+        gradientView.mask = label
     }
-
-
+    
+    @IBOutlet weak var gradientView: UIView!
+    @IBOutlet weak var label: UILabel!
+    
+    func addGradient() {
+        let gradient = CAGradientLayer()
+        gradient.frame = gradientView.bounds
+        gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.5)
+        gradientView.layer.addSublayer(gradient)
+    }
+    
+    func update() {
+        let motionEffectGroup = GradientMotionEffect()
+        motionEffectGroup.gradient = gradientView.layer.sublayers?.last as? CAGradientLayer
+        gradientView.addMotionEffect(motionEffectGroup)
+    }
+    
 }
 
+class GradientMotionEffect: UIMotionEffect {
+    var gradient: CAGradientLayer?
+    
+    override func keyPathsAndRelativeValues(forViewerOffset viewerOffset: UIOffset) -> [String : Any]? {
+        gradient?.startPoint.x = viewerOffset.horizontal
+        return nil
+    }
+    
+}
